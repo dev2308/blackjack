344fff90ef9318960a1844c150becb48552a6230
legendarysporks <evan@fitzpatricksr.us>
2020-06-12 14:10:36 -0600

    Code to run player hand and dealer hand

--- src/main/Blackjack.java
+++ src/main/Blackjack.java
@@ -10,23 +10,99 @@ public class Blackjack {
     private static ArrayList<String> dealerCards = new ArrayList<>();
     public static void main(String[] args) {
         String[] deck = makeDeck(suits, faces);
-	    ArrayList<String> shuffled = new ArrayList<>(shuffleDeck(deck));
-        playOneRound(shuffled);
-        //System.out.println("\n" + shuffled.length);
-        //hidden card: ▮
+		boolean playing = true;
+	    Scanner scan1 = new Scanner(System.in);
+		while(playing){
+			playOneRound(new ArrayList<>(shuffleDeck(deck)));
+			System.out.println("Play Again? (Y or N)");
+
+			String response = scan1.next();
+			if(!response.equalsIgnoreCase("y")){
+				playing = false;
+			}
+			playerCards.clear();
+			dealerCards.clear();
+		}
+	    scan1.close();
     }
 
     static void playOneRound(ArrayList<String> deck){
         deck = dealCards(deck);
-        printCards();
-
+        printCardsHidden();
+		deck = runPlayerHand(deck);
+		printCards();
+		deck = runDealerHand(deck);
+		printResult(getHandScore(playerCards), getHandScore(dealerCards));
     }
 
-    static void printCards(){
+    private static ArrayList<String> runPlayerHand(ArrayList<String> deck){
+    	Scanner scan = new Scanner(System.in);
+    	boolean acceptingCards = true;
+    	while(acceptingCards && getHandScore(playerCards) <= 21){
+    		System.out.println("Hit or Knock (Enter H or K)");
+    		String response = scan.next();
+    		if(response.equals("H")){
+    			playerCards.add(deck.remove(0));
+			    System.out.flush();
+			    printCardsHidden();
+		    }
+    		else{
+    			acceptingCards = false;
+		    }
+	    }
+    	return deck;
+    }
+
+    private static ArrayList<String> runDealerHand(ArrayList<String> deck){
+    	while(getHandScore(dealerCards) < 17){
+			dealerCards.add(deck.remove(0));
+		    System.out.flush();
+		    printCards();
+	    }
+    	return deck;
+    }
+
+	private static String[] makeDeck(String[] suits, String[] faces){
+		String[] unshuffled = new String[52];
+		int ind = 0;
+		for(String suit: suits){
+			for(String face: faces){
+				unshuffled[ind++] = face + suit;
+			}
+		}
+		return unshuffled;
+	}
+
+	private static ArrayList<String> dealCards(ArrayList<String> deck){
+		for(int i = 0; i < 2; i ++){
+			playerCards.add(deck.remove(0));
+			dealerCards.add(deck.remove(0));
+		}
+		return deck;
+	}
+
+	private static List<String> shuffleDeck(String[] unShuffled){
+		List<String> deckList = Arrays.asList(unShuffled);
+		Collections.shuffle(deckList);
+		return deckList;
+	}
+
+
+
+	private static void printCards(){
+		System.out.println("|----Your Cards-----|---Dealer's Cards--|");
+		System.out.print("|");
+		printHand(playerCards);
+		printHand(dealerCards);
+		System.out.println("\nYour Score: " + getHandScore(playerCards) + "\nDealer Score: " + getHandScore(dealerCards));
+	}
+
+    static void printCardsHidden(){
         System.out.println("|----Your Cards-----|---Dealer's Cards--|");
         System.out.print("|");
         printHand(playerCards);
         printDealerHand(dealerCards);
+        System.out.println("\nYour Score: " + getHandScore(playerCards));
     }
 
     static void printHand(ArrayList<String> hand){
@@ -47,34 +123,54 @@ public class Blackjack {
         System.out.print("|");
     }
 
+    static int getHandScore(ArrayList<String> hand){
+    	int sum = 0;
+    	for(String card: hand){
+			sum += parseCardScore(card);
+	    }
+    	return sum;
+    }
+
+    static int parseCardScore(String card){
+    	switch(card.charAt(0)){
+		    case 'A':
+			    return 1;
+		    case 'K':
+		    	return 10;
+		    case 'Q':
+		    	return 10;
+		    case 'J':
+			    return 10;
+		    case '1':
+		    	if(card.length() > 2){
+		    		return 10;
+			    }
+		    	else{
+		    		return 1;
+			    }
+		    default:
+			    return card.charAt(0) - 48;
+		    }
+
+    }
+
+	private static void printResult(int playerScore, int dealerScore){
+    	System.out.println("#########################");
+		if(playerScore == dealerScore || (playerScore > 21 && dealerScore > 21)){
+			System.out.println("Push");
+		}
+		else if(playerScore > 21 || (dealerScore > playerScore && dealerScore <= 21)){
+			System.out.println("Dealer Wins");
+		}
+		else{
+			System.out.println("Player Wins!");
+		}
+		System.out.println("#########################");
+	}
+
     static void printArray(String[] arr){
         for(String asdf: arr){
             System.out.print(asdf + " ");
         }
-    }
-
-    private static ArrayList<String> dealCards(ArrayList<String> deck){
-        for(int i = 0; i < 2; i ++){
-            playerCards.add(deck.remove(0));
-            dealerCards.add(deck.remove(0));
-        }
-        return deck;
-    }
-
-    private static String[] makeDeck(String[] suits, String[] faces){
-        String[] unshuffled = new String[52];
-        int ind = 0;
-        for(String suit: suits){
-            for(String face: faces){
-                unshuffled[ind++] = face + suit;
-            }
-        }
-        return unshuffled;
-    }
-
-    private static List<String> shuffleDeck(String[] unShuffled){
-    	List<String> deckList = Arrays.asList(unShuffled);
-	    Collections.shuffle(deckList);
-    	return deckList;
     }
 }
